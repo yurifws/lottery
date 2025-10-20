80fc70365ec8a56086e160f1aae27ef2b5986eb6
yurifws <yurifws@gmail.com>
2025-10-20 18:41:06 -0300

    <feat ES-73: implement players>

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,9 +2,15 @@ pragma solidity ^0.8.19;
 
 contract Lottery {
     address public manager;
+    address[] public players;
+
 
     constructor() public {
         manager = msg.sender;
     }
-    
+
+    function enter() public payable {
+        players.push(msg.sender);
+    }
+
 }
