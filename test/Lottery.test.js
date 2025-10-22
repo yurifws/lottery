f2de4af4246a4820fe15c8a6ad0d079b3fdba7ba
yurifws <yurifws@gmail.com>
2025-10-21 23:39:46 -0300

    <feat ES-87 test entering lottery>

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -25,4 +25,20 @@ describe('Lottery', () => {
     it('deploys a contract', () => {
         assert.ok(lottery.options.address)
     });
+
+    
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.01', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+
+    });
 });
