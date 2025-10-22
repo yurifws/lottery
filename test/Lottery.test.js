e6f35d97614156b8a8ae1b0aff9e415df3321470
yurifws <yurifws@gmail.com>
2025-10-21 23:19:42 -0300

    <feat ES-84> compile and deploy updates

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,1 +1,22 @@
-//test
+const assert = require('assert');
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+const web3 = new Web3(ganache.provider());
+ 
+const { abi, evm } = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+// Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+//Use one of those accounts to deploy
+//the contract
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object
+    })
+    .send({ from: accounts[0], gas: '1000000' });
+});
