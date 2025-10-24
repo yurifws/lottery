a8988ed9e9256c48542ed9cdacc0bf606488f83f
yurifws <yurifws@gmail.com>
2025-10-24 18:38:33 -0300

    <feat ES-107> store lastWinner to show in front end

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 contract Lottery {
     address public manager;
     address[] public players;
+    address public lastWinner;
 
     constructor() {
         manager = msg.sender;
@@ -20,6 +21,7 @@ contract Lottery {
     function pickWinner() public restricted {
         uint index = random() % players.length;
         payable(players[index]).transfer(address(this).balance);
+        lastWinner = players[index];
         players = new address[](0);
     }
 
